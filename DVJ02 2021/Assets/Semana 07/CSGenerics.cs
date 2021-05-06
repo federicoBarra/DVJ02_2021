8d797237c24b8fa1e1a09f4c4e5e0df58e102f76
federico_barra <federico.n.barra@gmail.com>
2021-05-05 22:12:44 -0300

    Semana 07

---
+++ DVJ02 2021/Assets/Semana 07/CSGenerics.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DVJ02.Semana07
+{
+	public class CSGenerics : MonoBehaviour
+	{
+		//public List<int> pepe;
+
+	    private void Start()
+	    {
+	        GenericMethod<int>(0);
+	        GenericMethod<string>("string");
+
+	        //SomeManager.Get().SomeMethod();
+	    }
+
+	    private void Update()
+	    {
+
+	    }
+
+	    private void GenericMethod<T>(T index)
+	    {
+	        T something = index;
+	        Debug.Log(something);
+	    }
+	}
+}
